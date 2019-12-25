f29d6c015e9f2cadddfe114dad795e46c9b8b201
JingPing.Liu <tonny_renner@163.com>
2019-12-25 10:35:25 +0800

    简单的适配react

--- src/index.js
+++ src/index.js
@@ -1,12 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import SingleSpaReact from 'single-spa-react';
+
 import App from './App';
-import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+require('./index.css');
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+// const App = () => {
+//   return <h1>Test for single-spa.</h1>
+// }
+
+const reactLifecycles = SingleSpaReact({
+  React,
+  ReactDOM,
+  rootComponent: App,
+  domElementGetter: () => document.getElementById("root")
+});
+
+
+export const bootstrap = reactLifecycles.bootstrap;
+export const mount = reactLifecycles.mount;
+export const unmount = reactLifecycles.unmount;
